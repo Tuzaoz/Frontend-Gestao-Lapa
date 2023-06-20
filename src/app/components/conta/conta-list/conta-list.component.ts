55751a2bd6151411444f5b07b541b5f501c2ef37
Arthur Dantas <arthurdantaspro@gmail.com>
2023-06-20 18:34:44 -0300

    filtro de data para vendas e contas funcional

--- src/app/components/conta/conta-list/conta-list.component.ts
+++ src/app/components/conta/conta-list/conta-list.component.ts
@@ -7,23 +7,35 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {ContaAddComponent} from "../../conta/conta-add/conta-add.component";
 import {ContaUpdateComponent} from "../../conta/conta-update/conta-update.component";
+import {MatDatepickerInputEvent} from "@angular/material/datepicker";
+import {DateAdapter, MAT_DATE_FORMATS} from "@angular/material/core";
+import {GRI_DATE_FORMATS} from "../../vendas/vendas-list/vendas-list.component";
 
 @Component({
   selector: 'app-conta-list',
   templateUrl: './conta-list.component.html',
-  styleUrls: ['./conta-list.component.css']
+  styleUrls: ['./conta-list.component.css'],
+  providers: [
+    { provide: MAT_DATE_FORMATS, useValue: GRI_DATE_FORMATS },
+  ]
 })
 export class ContaListComponent {
 
   ELEMENT_DATA: Conta[] = [];
   constructor(
     private service: ContaService,
-    public dialog: MatDialog
-  ) {}
+    public dialog: MatDialog,
+    private readonly adapter: DateAdapter<Date>
+
+  ) {this.adapter.setLocale("pt-BR")}
   displayedColumns: string[] = ['id','nomeConta', 'valor', 'data', 'acoes'];
   dataSource = new MatTableDataSource<Conta>(this.ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort
+  dataBusca:string;
+  dataHead: string;
+
+
   openDialog(): void {
     const dialogRef = this.dialog.open(ContaAddComponent, {width: '500px'});
   }
@@ -53,6 +65,35 @@ export class ContaListComponent {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+  formatDate(dateString: string): string {
+    const [year, month, day] = dateString.split('-');
+    return `${day}/${month}/${year}`;
+  }
+  formatDate2(dateString: string): string {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+
+    return `${year}-${month}-${day}`;
+  }
+
+  addEvent(event: MatDatepickerInputEvent<Date>) {
+    this.dataBusca = this.formatDate2(event.value.toISOString());
+    this.aplicarFiltro();
+    console.log(this.dataBusca);
+  }
+  aplicarFiltro(){
+    this.service.findByDate(this.dataBusca).subscribe(response =>{
+        this.ELEMENT_DATA = [];
+        this.ELEMENT_DATA = response;
+        this.dataSource = new MatTableDataSource<any>(response);
+        this.dataHead = this.formatDate(this.dataBusca);
+      }
+
+
+    )
   }
 
 }
--- src/app/components/vendas/vendas-list/vendas-list.component.ts
+++ src/app/components/vendas/vendas-list/vendas-list.component.ts
@@ -9,23 +9,41 @@ import {VendasAddComponent} from "../../vendas/vendas-add/vendas-add.component";
 import {VendasUpdateComponent} from "../../vendas/vendas-update/vendas-update.component";
 import {Produto} from "../../../models/produto";
 import {ProdutoService} from "../../../services/produto.service";
+import {MatDatepickerInputEvent} from "@angular/material/datepicker";
+import {DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, MatDateFormats} from "@angular/material/core";
 
+export const GRI_DATE_FORMATS: MatDateFormats = {
+  ...MAT_NATIVE_DATE_FORMATS,
+  display: {
+    ...MAT_NATIVE_DATE_FORMATS.display,
+    dateInput: {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    } as Intl.DateTimeFormatOptions,
+  }
+};
 @Component({
   selector: 'app-vendas-list',
   templateUrl: './vendas-list.component.html',
-  styleUrls: ['./vendas-list.component.css']
+  styleUrls: ['./vendas-list.component.css'],
+  providers: [
+    { provide: MAT_DATE_FORMATS, useValue: GRI_DATE_FORMATS },
+  ]
 })
 export class VendasListComponent implements OnInit{
   ELEMENT_DATA: Venda[] = [];
   constructor(
     private service: VendaService,
     public dialog: MatDialog,
-  ) {}
+    private readonly adapter: DateAdapter<Date>
+  ) {this.adapter.setLocale("pt-BR")}
   displayedColumns: string[] = ['id','nomeCliente', 'produto', 'valor', 'metodoPagamento', 'acoes'];
   dataSource = new MatTableDataSource<Venda>(this.ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort
-
+  dataHead:string = this.formatDate(this.formatDate2(new Date().toISOString()));
+  dataBusca:string
 
   openDialog(): void {
     const dialogRef = this.dialog.open(VendasAddComponent, {width: '500px'});
@@ -46,12 +64,36 @@ export class VendasListComponent implements OnInit{
   ngOnInit(): void {
     this.findHoje();
   }
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+  formatDate(dateString: string): string {
+    const [year, month, day] = dateString.split('-');
+    return `${day}/${month}/${year}`;
   }
+  formatDate2(dateString: string): string {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+
+    return `${year}-${month}-${day}`;
+  }
+
+  addEvent(event: MatDatepickerInputEvent<Date>) {
+    this.dataBusca = this.formatDate2(event.value.toISOString());
+    this.aplicarFiltro();
+  }
+  aplicarFiltro(){
+    this.service.findByDate(this.dataBusca).subscribe(response =>{
+        this.ELEMENT_DATA = [];
+        this.ELEMENT_DATA = response;
+        this.dataSource = new MatTableDataSource<any>(response);
+        this.dataHead = '';
+        this.dataHead = this.formatDate(this.dataBusca);
+
+      }
+
+
+    )
+  }
+
 
 }
